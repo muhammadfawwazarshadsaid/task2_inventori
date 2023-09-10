9c377ec7f74f1d2898492ebeb1ef50922df88c74
muhammadfawwazarshads <muhammad.fawwaz22@ui.ac.id>
2023-09-10 22:26:17 +0700

    meowww

--- main/views.py
+++ main/views.py
@@ -2,10 +2,11 @@ from django.shortcuts import render
 
 def show_main(request):
     context = {
-        'name': 'Pak Bepe',
-        'description': 'Kandang Kucing',
+        'no': 1,
+        'name': 'Kandang kucing',
+        'description': 'Kandang kucing anti jebol dan anti bau.',
         'price': 20000,
-        'amount': 5
+        'amount': 92
     }
 
     return render(request, "main.html", context)
