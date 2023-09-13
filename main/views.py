eef06b24cdb13705dfbc47c0693746171bfcd73e
muhammadfawwazarshads <muhammad.fawwaz22@ui.ac.id>
2023-09-13 11:32:27 +0700

    adj

--- main/views.py
+++ main/views.py
@@ -1,12 +1,29 @@
 from django.shortcuts import render
 
 def show_main(request):
-    context = {
-        'no': 1,
-        'name': 'Kandang kucing',
-        'description': 'Kandang kucing anti jebol dan anti bau.',
-        'price': 20000,
-        'amount': 92
-    }
+    context = [
+        {
+            'no': 1,
+            'name': 'Kandang Kucing',
+            'description': "Kandang kucing dengan anti karat dan anti bau",
+            'amount': 5,
+            'price': 74000,
+        },
+        {
+            'no': 2,
+            'name': 'Pasir 200g',
+            'description': "Pasir putih anti lengket",
+            'amount': 91,
+            'price': 10000,
+        },
+        {
+            'no': 3,
+            'name': 'Royal Candi 1kg',
+            'description': "Makanan kucing berkualitas premium",
+            'amount': 241,
+            'price': 240000,
+        },
+        # Add more dictionaries as needed
+    ]
 
-    return render(request, "main.html", context)
+    return render(request, "main.html", {'items': context})
