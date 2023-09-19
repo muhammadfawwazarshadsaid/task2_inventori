fb654802d644219464aee4f085431004bcdd5ba7
muhammadfawwazarshads <muhammad.fawwaz22@ui.ac.id>
2023-09-20 02:24:11 +0700

    new branch task 3

--- main/models.py
+++ main/models.py
@@ -2,6 +2,7 @@ from django.db import models
 
 class Product(models.Model):
     no = models.AutoField(primary_key=True)
+    date = models.DateField(auto_now_add=True)
     name = models.CharField(max_length=255)
     description = models.TextField()
     price = models.PositiveIntegerField()
