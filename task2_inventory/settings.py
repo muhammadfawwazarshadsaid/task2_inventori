ffc8596ea1cc2bbb4043b5fa0ceea2b0366e7924
muhammadfawwazarshads <muhammad.fawwaz22@ui.ac.id>
2023-09-24 02:09:01 +0700

    reupdate yml setting

--- task2_inventory/settings.py
+++ task2_inventory/settings.py
@@ -16,6 +16,7 @@ import os # Tambahkan kode berikut
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+env = environ.Env() # Tambahkan kode berikut
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
@@ -24,16 +25,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-hkv3&+%s+t$$rx-9xwsg3qexf#9qajlvoh87jv@i%_p_*2!7b2'
 # Automatically determine environment by detecting if DATABASE_URL variable.
 # DATABASE_URL is provided by Heroku if a database add-on is added (e.g. Heroku Postgres).
-import os
-import environ
-
-env = environ.Env()
-
-# Access the 'PRODUCTION' environment variable and convert it to a boolean
-PRODUCTION = env.bool('PRODUCTION', default=False)
-
 # Automatically determine environment by detecting if DATABASE_URL variable.
 # DATABASE_URL is provided by Heroku if a database add-on is added (e.g. Heroku Postgres).
+PRODUCTION = env.bool('PRODUCTION', False)
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -97,7 +92,7 @@ DATABASES = {
 # Set database settings automatically using DATABASE_URL.
 if PRODUCTION:
     DATABASES = {
-        'default': environ.db('DATABASE_URL')
+        'default': env.db('DATABASE_URL')
     }
     DATABASES["default"]["ATOMIC_REQUESTS"] = True
 
@@ -135,10 +130,6 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
-
-STATIC_URL = 'static/'
-# Default primary key field type
-# https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 STATIC_URL = 'static/'
 
